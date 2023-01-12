3af961e28347d4135811b13739edea00614e9d70
andreistan26 <andreistan2003@gmail.com>
2023-01-12 21:32:52 +0200

    complete windows

--- windows.c
+++ windows.c
@@ -1,18 +1,17 @@
 #include "windows.h"
 
-WordleWinBundle *init_win_bundle(size_t size, void (*draw_wins)(WordleWinBundle *)){
-    WordleWinBundle *bundle = malloc(sizeof(WordleWinBundle));
+WinBundle *init_win_bundle(size_t size){
+    WinBundle *bundle = malloc(sizeof(WinBundle));
     bundle->wins = malloc(size * sizeof(WINDOW *));
     for(size_t i = 0; i < size; i++){
         bundle->wins[i] = NULL;
     }
     bundle->size = size;
-    bundle->draw_wins = draw_wins;
 
     return bundle;
 }
 
-void draw_logo(WordleWinBundle *logo_win){
+void draw_logo(WinBundle *logo_win){
     //TODO add logo from config
     static const char *logo[6] = {"W", "O", "R", "D", "L", "E"};
     static const Pointf pos_scale = {.x = 0.5, .y = 0.1};
@@ -39,12 +38,13 @@ void draw_logo(WordleWinBundle *logo_win){
     wrefresh(logo_win->wins[0]);
 }
 
-void draw_guesses(WordleWinBundle *input_wins){
+void draw_guesses(WinBundle *input_wins, word **words){
     static const Pointf pos_scale = {.x = 0.5, .y = 0.4};
     static const Point input_dim = {.x = 5 + 2, .y = 1 + 2};
     static const int vertical_gap = 1;
     //TODO add guesses from config
     static const int guesses = 6;
+    static const int word_len = 5;
     Point display_size;
     getmaxyx(stdscr, display_size.y, display_size.x);
     
@@ -54,12 +54,17 @@ void draw_guesses(WordleWinBundle *input_wins){
                 display_size.x * pos_scale.x - input_dim.x * 0.5,
                 display_size.y * pos_scale.y + i * input_dim.y + vertical_gap
         );
+        for(size_t word_it = 0; word_it < words[i]->len; word_it++){
+            attron(COLOR_PAIR(words[i]->color[word_it]));
+            mvwaddch(input_wins->wins[i], 1, word_it + 1, words[i]->value[word_it]);
+            attroff(COLOR_PAIR(words[i]->color[word_it]));
+        }
         box(input_wins->wins[i], 0, 0);
         wrefresh(input_wins->wins[i]);
     }
 } 
 
-void draw_keyboard(WordleWinBundle *keyboard_wins){
+void draw_keyboard(WinBundle *keyboard_wins, int color[ALPH_SIZE]){
     static const Pointf pos_scale = {.x = 0.5, .y = 0.4};
     static const Point key_dim = {.x = 1 + 2, .y = 1 + 2};
     static const Point gap = {.x = 1, .y = 1};
@@ -69,31 +74,41 @@ void draw_keyboard(WordleWinBundle *keyboard_wins){
     getmaxyx(stdscr, display_size.y, display_size.x);
     
     WINDOW **it = keyboard_wins->wins;
-    for(size_t row = 0; row < querty_rows; row++){
-        for(size_t col = 0; col < sizeof(querty_layout[row]); col++){
-            *it = newwin(
-                    key_dim.y, key_dim.x,
-                    display_size.x * pos_scale.x - sizeof(querty_layout[row]) * key_dim.x * 0.5 + gap.x + key_dim.x * col,
-                    display_size.y * pos_scale.y + row * key_dim.y + gap.y
-            );
-            box(*it, 0, 0);
-            wrefresh((*it));
-            it++;
-        }
+    size_t row = 1;
+    for(size_t key_idx = 0; key_idx < ALPH_SIZE; key_idx++){
+        if(key_idx > keys_per_row[row] - 1)
+            row++;
+        *it = newwin(
+                key_dim.y, key_dim.x,
+                display_size.x * pos_scale.x - keys_per_row[row] * key_dim.x * 0.5 + gap.x + key_dim.x * (keys_per_row[row] - key_idx),
+                display_size.y * pos_scale.y + row * key_dim.y + gap.y
+        );
+        box(*it, 0, 0);
+        wrefresh(*it);
+        it++;
     }
 }
 
-void draw_all_windows(WordleWinBundle **wins){
-    clear();
-
-    for(size_t i = 0; i < 3; i++){
-        wins[i]->draw_wins(wins[i]);
+void draw_col_word(WinBundle *word_win, size_t idx, word *word){
+    WINDOW **win = &(word_win->wins[idx]);
+    Point pos;
+    getmaxyx(*win, pos.y, pos.x);
+    for(size_t i = 0; i < word->len; i++){
+        attron(COLOR_PAIR(word->color[i]));
+        mvwaddch(*win, 1, 1, word->value[i]);
+        attroff(COLOR_PAIR(word->color[i]));
     }
-
-    refresh();
+    wrefresh(*win);
 }
 
-
-
+void draw_col_key(WinBundle *keyboard_win, char key, int color){
+    WINDOW **key_win = &(keyboard_win->wins[key_2_idx[KEY_ALPH_IDX(key)]]);
+    Point pos;
+    getmaxyx(*key_win, pos.y, pos.x);
+    attron(COLOR_PAIR(color));
+    mvwaddch(*key_win, 1, 1, key);
+    attroff(COLOR_PAIR(color));
+    wrefresh(*key_win);
+}
 
 
--- windows.h
+++ windows.h
@@ -4,8 +4,12 @@
 
 #define ALPH_SIZE       26
 #define WINDOW_TYPES    3
-
-static const char *querty_layout[3] = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+#define KEY_ALPH_IDX(key)((key) - 'a')
+static const char *querty_layout = {"qwertyuiopasdfghjklzxcvbnm"};
+// accesed with key - 'a'
+static const size_t key_2_idx[ALPH_SIZE] = {10, 23, 21, 12, 2, 13, 14, 15, 7, 16, 17, 18,
+                25, 24, 8, 9, 0, 3, 11, 4, 6, 22, 1, 20, 5, 19};
+static const int keys_per_row[3] = {10, 10 + 9, 10 + 9 + 7};
 
 
 typedef struct{
@@ -16,17 +20,15 @@ typedef struct{
     float x, y;
 }Pointf;
 
-typedef struct WordleWinBundle{
+typedef struct WinBundle{
     WINDOW **wins;
     size_t  size;
-    void  (*draw_wins)(struct WordleWinBundle *);
-}WordleWinBundle;
+}WinBundle;
 
-WordleWinBundle *init_win_bundle(size_t size, void (*draw_wins)(WordleWinBundle *));
-void draw_all_windows(WordleWinBundle **wins);
-void draw_keyboard(WordleWinBundle *keyboard_wins);
-void draw_guesses(WordleWinBundle *guesses_wins);
-void draw_logo(WordleWinBundle *logo_win);
+WinBundle *init_win_bundle(size_t size);
+void draw_keyboard(WinBundle *keyboard_wins, int color[ALPH_SIZE]);
+void draw_guesses(WinBundle *guesses_wins, word **words);
+void draw_logo(WinBundle *logo_win);
 
-void draw_col_key(WordleWinBundle *keyboard_wins, char key, size_t color);
-void draw_col_word(WordleWinBundle *keyboard_wins, size_t idx, word *word, size_t color);
+void draw_col_key(WinBundle *keyboard_win, char key, int color);
+void draw_col_word(WinBundle *word_win, size_t idx, word *word);
